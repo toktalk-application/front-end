578696ffcf8bb4d134a68fbf5b1a2e5ecfb62325
whgkswo <gjuk7238@gmail.com>
2024-10-15 11:25:11 +0900

    feat FixWrongErrorMessage(#129)

--- src/axios/DeleteRequest.js
+++ src/axios/DeleteRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import {REACT_APP_API_URL} from '@env';
+import handleErrorMessage from "./ErrorMessageHandler";
 
-const sendDeleteRequest = async({ token, endPoint, requestParams, onSuccess, onFailure }) => {
+const sendDeleteRequest = async({ token, endPoint, requestParams, onSuccess, onFailure, disableDefaultAlert }) => {
     /* console.log("token: ", token);
     console.log("endPoint: ", endPoint);
     console.log("requestParams: ", requestParams); */
@@ -17,16 +18,18 @@ const sendDeleteRequest = async({ token, endPoint, requestParams, onSuccess, onF
                 }
             },
         )
-        if(response.status === 204){
+        if(response.status === 200 || response.status === 204){
             if(onSuccess) onSuccess(response.data);
         }
     }catch(error){
+        const errorStatus = error.response.status;
+        const errorMessage = error.response.data.message;
         console.error('요청 실패: ', error);
         console.error('message: ', error.message);
-        console.error('server message: ', error.response.data.message);
+        console.error('server message: ', errorMessage);
 
-        handleErrorMessage(error.response.status, error.response.data.message);
-        if(onFailure) onFailure();
+        if(!disableDefaultAlert) handleErrorMessage(errorStatus, errorMessage);
+        if(onFailure) onFailure(errorStatus, errorMessage);
     }
 }
 export default sendDeleteRequest;
